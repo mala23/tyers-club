afed2a4f735712b106c3701c304029e07a5edcb2
Colin Nico Schmid <mail@mala23.me>
2018-03-03 17:47:59 +0100

    style text

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  typekit: {
+    id: 'udz2ped'
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
