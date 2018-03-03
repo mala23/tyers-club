50fd6e4f63b806ebcc1bd97801b721c1232ac5a2
Colin Nico Schmid <mail@mala23.me>
2018-03-03 12:21:50 +0100

    put content in place

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="Tyers Club Logo" />
+          <h1 className="app-title">Tyers Club</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="app-intro">
+          A smart Web App that allows fly tyers and fly fishermen to hold an 
+      effortless inventory of their materials, browse and share patterns and 
+      see possible patterns based on their inventory. Might even hold 
+      information for what fly works for what species at what location and at 
+      which times.
         </p>
       </div>
     );
