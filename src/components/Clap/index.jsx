cce0a5f3cdc322ee235c97df2c1d0b371161e899
Colin Nico Schmid <mail@mala23.me>
2018-03-05 12:09:21 +0100

    put count in place

--- src/components/Clap/index.jsx
+++ src/components/Clap/index.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import styles from './clap.module.css'
 
 class Clap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 23,
+    }
+  }
+
   render() {
     return (
       <article className={styles.text}>
-        <p>23</p>
+        <p>{this.state.count}</p>
         <section className="clap-buttons">
           <button>Clap</button>
         </section>
